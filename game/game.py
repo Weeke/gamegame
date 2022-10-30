0f79d6ec4b6ca6e4c0b28205654d54b4584b421f
Timur <weekenm3@icloud.com>
2022-10-30 08:59:23 +0100

    game

---
+++ game/game.py
@@ -0,0 +1,9 @@
+import random
+ina = input("Введите действие:камень ножницы бумага")
+def game(ch, result):
+choice = input("Введите действие:камень ножницы бумага")
+def game(choice, result):
+    print("")
+def game(choice, result):
+    computer_choice = random.choice("кнж")
+computer_choice = random.choice("кнб")
